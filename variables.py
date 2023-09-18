28254de1dbe3de53839ef0ec17f3e4f18a9de8c4
Mohana M <145038131+somuchmoh@users.noreply.github.com>
2023-09-18 18:36:30 +0530

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,12 @@
+name = "Jack"
+print(name)
+
+name = "Mohana"
+print(name)
+
+name = (input("What is your name? "))
+print (name)
+
+#use proper naming for variables - eg: name, user_name, etc
+user_name = input("What is your name? ")
+print("Hey there, " + user_name)
